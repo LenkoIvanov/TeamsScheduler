5ee79ce32f03e8102a943f29fe8e5603dbd17642
Lenko Ivanov <lenko.ivanov117@gmail.com>
2024-06-04 23:13:32 +0300

    feat: login with a microsoft account

--- react-scheduler/src/App.tsx
+++ react-scheduler/src/App.tsx
@@ -1,12 +1,21 @@
 import { AppContainer } from "./components/app_container/AppContainer";
 import { Header } from "./components/header/Header";
 import styles from "./App.module.scss";
+import { Login, useIsSignedIn } from "@microsoft/mgt-react";
 
 function App() {
+  const [isSignedIn] = useIsSignedIn();
+
   return (
     <div className={styles.app}>
-      <Header />
-      <AppContainer />
+      {isSignedIn ? (
+        <>
+          <Header />
+          <AppContainer />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
--- react-scheduler/src/main.tsx
+++ react-scheduler/src/main.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Msal2Provider } from "@microsoft/mgt-msal2-provider";
+import { Providers } from "@microsoft/mgt-element";
 import "./index.scss";
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  scopes: [
+    "calendars.read",
+    "user.read",
+    "openid",
+    "profile",
+    "people.read",
+    "user.readbasic.all",
+  ],
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
