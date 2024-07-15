a3bbe660edfc0f658258939e8ed559b952ca6856
blubito-lenko-ivanov-01 <l.ivanov@blubito.com>
2024-07-15 14:54:02 +0300

    feat: import first batch of room status components

---
+++ vue-scheduler/src/components/room_status_components/ColorWaves.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+interface ColorWavesProps {
+  isBooked: boolean;
+}
+const props = defineProps<ColorWavesProps>();
+const { isBooked } = props;
+</script>
+
+<template>
+  <div :class="$style.colorWaveWrapper">
+    <div :class="[$style.colorWave, $style.firstChild, isBooked ? $style.firstBooked : '']"></div>
+    <div :class="[$style.colorWave, $style.secondChild]"></div>
+    <div :class="[$style.colorWave, $style.thirdChild, isBooked ? $style.thirdBooked : '']"></div>
+  </div>
+</template>
+
+<style lang="scss" module>
+.colorWaveWrapper {
+  height: 100%;
+  width: 100%;
+  position: relative;
+
+  .colorWave {
+    position: absolute;
+    inset: 0px;
+    border: 10px solid transparent;
+    border-radius: 115% 140% 145% 110%/125% 140% 110% 125%;
+    mix-blend-mode: screen;
+
+    &.firstChild {
+      border-color: var(--light-green);
+      transform-origin: 50% 50%;
+      animation: spin1 5.5s linear infinite;
+
+      &.firstBooked {
+        border-color: var(--light-red);
+      }
+    }
+
+    &.secondChild {
+      border-width: 15px;
+      border-color: var(--light-purple);
+      transform-origin: 50% 50%;
+      animation: spin2 5.5s linear infinite;
+    }
+
+    &.thirdChild {
+      border-color: var(--dark-green);
+      transform-origin: 50% 50%;
+      animation: spin3 5.5s linear infinite;
+
+      &.thirdBooked {
+        border-color: var(--dark-red);
+      }
+    }
+  }
+}
+
+@keyframes spin1 {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes spin2 {
+  0% {
+    transform: rotate(72deg);
+  }
+
+  100% {
+    transform: rotate(-288deg);
+  }
+}
+
+@keyframes spin3 {
+  0% {
+    transform: rotate(-144deg);
+  }
+
+  100% {
+    transform: rotate(216deg);
+  }
+}
+</style>
---
+++ vue-scheduler/src/components/room_status_components/RoomStatus.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import ColorWaves from './ColorWaves.vue';
+import type { EventInfo } from '@/types/EventInfo';
+import { ref } from 'vue';
+
+interface RoomStatusProps {
+  events: EventInfo[];
+  currentEvent: EventInfo | null;
+  currentMoment: Date;
+  isLoading: boolean;
+  isError: boolean;
+}
+const props = defineProps<RoomStatusProps>();
+const { events, currentEvent, currentMoment, isLoading, isError } = props;
+
+const initProgressBarValue = 100;
+const progressBarValue = ref(100);
+</script>
+
+<template>
+  <div :class="$style.roomStatusWrapper">
+    <ColorWaves :isBooked="false" />
+  </div>
+</template>
+
+<style lang="scss" module>
+.roomStatusWrapper {
+  width: 100%;
+  justify-self: center;
+  aspect-ratio: 1/1;
+  position: relative;
+  padding: 0.75rem;
+  color: var(--clr-white);
+  overflow: hidden;
+}
+</style>
